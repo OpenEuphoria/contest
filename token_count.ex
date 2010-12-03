763ae10c01698c00cbe1d0dcec7b9628e89e1e4b
Jeremy Cowgar <jeremy@cowgar.com>
2010-12-03 11:24:47 -0500

    Added filename globbing for the token counter

--- token_count.ex
+++ token_count.ex
@@ -4,14 +4,37 @@
 
 include std/sort.e
 include std/math.e
+include std/filesys.e
+include std/sequence.e
+include std/utils.e
+include std/console.e
+
 include euphoria/tokenize.e
 
 enum NAME, TOKENS
+
 sequence stats = {}
 
-sequence cmds = command_line()
+function glob(sequence pattern)
+	return vslice(iff(atom(dir(pattern)), "", dir(pattern)), D_NAME)
+end function
+
+function glob_patterns(sequence patterns)
+	sequence result = {}
+	
+	for i = 1 to length(patterns) do
+		result &= glob(patterns[i])
+	end for
+	
+	return result
+end function
+
+sequence cmds = glob_patterns(command_line())
 for i = 3 to length(cmds) do
 	sequence fname = cmds[i]
+	if equal(fname, ".") or equal(fname, "..") then
+		continue
+	end if
 	
 	sequence tokens = tokenize:tokenize_file(fname)
 	stats &= { { fname, length(tokens[1]) } }
@@ -22,7 +45,7 @@ integer
 	twenty_percent = ceil(submission_count * 0.2),
 	did_divider = 0
 
-stats = sort_columns(stats, { 2 })
+stats = sort_columns(stats, { 2, 1 })
 
 printf(1, "Pos | Tok # | Filename\n", {})
 printf(1, "---------------------------------------------------\n", {})
@@ -36,5 +59,3 @@ for submission = 1 to length(stats) do
 	
 	printf(1, "%3d | %5d | %s\n", { submission, sub[TOKENS], sub[NAME] })
 end for
-
---? { submission_count, twenty_percent }
